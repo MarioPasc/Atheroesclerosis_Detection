d5cbe8fca2e009d4a6d65212b4d9a199edeaa98e
MarioPasc <mario.pg02@gmail.com>
2024-06-20 10:58:10 +0200

    Validacion en modelo yolov8

--- src/models/yolov8.py
+++ src/models/yolov8.py
@@ -1,5 +1,8 @@
 import ultralytics
 import os
+import glob
+import pandas as pd
+
 
 class Detection_YOLOv8:
 
@@ -18,10 +21,57 @@ class Detection_YOLOv8:
                                          scale=0.0, shear=0.0, perspective=0.0, flipud=0.0, fliplr=0.0, mosaic=0.0,
                                          close_mosaic=0, mixup=0.0, copy_paste=0.0, auto_augment="", erasing=0.0, batch=4)
 
+    def val(self) -> None:
+        weights_folder = "./runs/detect/ateroesclerosis_training/weights"
+        results_list = []
+
+        # Get all the weight files, excluding 'last.pt'
+        weight_files = sorted([file for file in glob.glob(os.path.join(weights_folder, '*.pt')) if 'last.pt' not in file])
+        weight_files.append(os.path.join(weights_folder, 'last.pt'))  # Include last.pt
+        weight_files.insert(0, os.path.join(weights_folder, 'best.pt'))  # Include best.pt
+
+        for epoch, weight in enumerate(weight_files):
+            if os.path.basename(weight) == 'best.pt':
+                epoch_num = 0
+            elif os.path.basename(weight) == 'last.pt':
+                epoch_num = 100
+            else:
+                epoch_num = int(os.path.basename(weight).replace('epoch', '').replace('.pt', ''))
+
+            model_batch = ultralytics.YOLO(weight)
+            results = model_batch.val(imgsz=640, conf=0.01, plots=True)
+
+            # Collect results
+            precision_b = results.box.mp  # precisión media
+            recall_b = results.box.mr  # recall medio
+            map_05_b = results.box.map50  # mAP para th=.5
+            map_05_95_b = results.box.map  # mAP para th=.5-.95
+
+            results_list.append({
+                "epoch": epoch_num,
+                "weight": os.path.basename(weight),
+                "precision": precision_b,
+                "recall": recall_b,
+                "map_05": map_05_b,
+                "map_05_95": map_05_95_b
+            })
+
+        # Save results to a CSV file
+        results_df = pd.DataFrame(results_list)
+        results_df.to_csv(os.path.join('./runs/detect/ateroesclerosis_training', 'validation_results.csv'), index=False)
+        print("Validation results saved to 'validation_results.csv'")
+
+
+
+
+            
+
+
 
 def main() -> int:
     model = Detection_YOLOv8(model_path="", yaml_path="./config.yaml")
     model.train()
+    model.val()
     return 0
 
 if __name__ == "__main__":
